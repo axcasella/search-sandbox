2c62e0adf76bee6127d16667f0f66a4d5e916868
axcasella <axc@bu.edu>
2023-06-22 23:26:08 -0400

    simple chatgpt chat bot example

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-import { Configuration, Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const configuration = new Configuration({
-  organization: "",
-  apiKey: "",
+  organization: "org-OqXxaHHtx4RLvFS592zWYq9r",
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/", async(req, res) => {
+  const { message } = req.body;
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {role: "user", content: `${message}`},
+    ]
+  });
+
+  res.json({
+    completion: completion.data.choices[0].message,
+  })
+})
+
+app.listen(port, () => {
+  console.log("app running on port", port);
+})
