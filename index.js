802e1dc22b619935f28e9fb24548e4503c06df6c
axcasella <axc@bu.edu>
2023-06-24 14:56:18 -0400

    added documents; updated dependencies

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
-import { Configuration, OpenAIApi } from "openai";
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { TextLoader } from "langchain/document_loaders/text";
+import { PDFLoader } from "langchain/document_loaders/pdf";
+import * as dotenv from "dotenv";
+import { createPineconeIndex } from "./1-createPineconeIndex.js";
+import { updatePinecone } from "./2-updatePinecone.js";
+import { queryPineconeVectorStoreAndQueryLLM } from "./3-queryPineconeVectorStoreAndQueryLLM.js";
 
-const configuration = new Configuration({
-  organization: "org-OqXxaHHtx4RLvFS592zWYq9r",
-  apiKey: "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",
+dotenv.config();
+
+const loader = new DirectoryLoader("./documents", {
+  ".txt": (path) => new TextLoader(path),
+  ".pdf": (path) => new PDFLoader(path),
 });
 
-const openai = new OpenAIApi(configuration);
+const docs = await loader.load();
 
-const app = express();
-const port = 3000;
+// import { Configuration, OpenAIApi } from "openai";
+// import express from "express";
+// import bodyParser from "body-parser";
+// import cors from "cors";
 
-app.use(bodyParser.json());
-app.use(cors());
+// const configuration = new Configuration({
+//   organization: "org-OqXxaHHtx4RLvFS592zWYq9r",
+//   apiKey: "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",
+// });
 
-app.post("/", async(req, res) => {
-  const { message } = req.body;
+// const openai = new OpenAIApi(configuration);
 
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {role: "user", content: `${message}`},
-    ]
-  });
+// const app = express();
+// const port = 3000;
 
-  res.json({
-    completion: completion.data.choices[0].message,
-  })
-})
+// app.use(bodyParser.json());
+// app.use(cors());
 
-app.listen(port, () => {
-  console.log("app running on port", port);
-})
+// app.post("/", async(req, res) => {
+//   const { message } = req.body;
+
+//   const completion = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: [
+//       {role: "user", content: `${message}`},
+//     ]
+//   });
+
+//   res.json({
+//     completion: completion.data.choices[0].message,
+//   })
+// })
+
+// app.listen(port, () => {
+//   console.log("app running on port", port);
+// })
